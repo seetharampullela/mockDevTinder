ae383cec6e0fb99414d9759a5eb6daca9fb992dd
seetharampullela <seetharampullela@gmail.com>
2026-03-16 12:22:18 +0530

    refactored adminAuth route

--- src/app.js
+++ src/app.js
@@ -2,12 +2,10 @@ const express = require("express");
 const { adminAuth } = require("./middlewares/auth");
 const app = express();
 
-app.use("/admin", (req, res, next) => {
-  adminAuth(req, res, next);
-});
+app.use("/admin", adminAuth);
 
 app.use("/admin/getUser", (req, res) => {
-  res.send("Admin getUser Route");
+  res.send("Get Admin user >>> Route");
 });
 
 // app.use("/user", (req, res) => {
