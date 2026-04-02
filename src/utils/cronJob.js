da60fa363b741623d45ce5da702766641aff6226
seetharampullela <seetharampullela@gmail.com>
2026-04-02 08:41:02 +0530

    refactored and added comments for cronJob

--- src/utils/cronJob.js
+++ src/utils/cronJob.js
@@ -3,9 +3,10 @@ const ConnectionRequest = require("../model/connectionRequest");
 const { subDays, startOfDay, endOfDay } = require("date-fns");
 const sendEmail = require("../utils/sendEmail");
 
+/* Scheduled this job to run at 8AM every morningff */
 cron.schedule("0 8 * * *", async () => {
   try {
-    const yesterday = subDays(new Date(), 1);
+    const yesterday = subDays(new Date(), 0);
     const startYesterday = startOfDay(yesterday);
     const endYesterday = endOfDay(yesterday);
 
@@ -16,9 +17,16 @@ cron.schedule("0 8 * * *", async () => {
         $lt: endYesterday,
       },
     }).populate("fromUserId toUserId");
-    const listOfEmails = new Set(
-      pendingRequests.map((req) => req.toUserId.emailId),
-    );
+    const listOfEmails = [
+      ...new Set(pendingRequests.map((req) => req.toUserId.emailId)),
+    ];
+    /* 
+      Instead of for-loop use bee-queue node module for this.
+      If there are so-many mails to be sent we cannot send like this as this would take a lot of time. 
+      Either we can let aws handle this one or we can queue this process throught js function.
+      1. npm i bullmq
+      2. npm i beeque
+    */
     for (const toEmailId of listOfEmails) {
       if (pendingRequests.length > 0) {
         const mailRes = await sendEmail.run(
